e5125be4086b63450678319e88704f0bf359d170
GitMacko <GitMacko@example.com>
2023-05-31 19:02:47 +0200

    Śledzenie wyboru użytkownika

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import Card from './components/Card.vue'
 import { ref }  from 'vue'
+import { watch } from 'vue'
 export default {
   name: 'WebMemoryGamev2',
   components: {
@@ -8,9 +9,11 @@ export default {
   },
   setup() {
     const TablicaKart = ref([])
+    const rekaGracza = ref([])
+
     for(let i = 0; i< 16; i++){
       TablicaKart.value.push({
-        value: i,
+        value: 5,
         visible:false,
         position: i
       
@@ -18,10 +21,37 @@ export default {
     }
     const odwrocKarte = (payload) => {
       TablicaKart.value[payload.position].visible = true
+      if(rekaGracza.value[0]){
+        if(
+      rekaGracza.value[0].position ===
+    payload.position && rekaGracza.value[0].faceValue === payload.faceValue)
+    { return } else { }
+        rekaGracza.value[1]= payload
+      } else {
+        rekaGracza.value[0] = payload
+      }
     }
+
+    watch(rekaGracza, currentValue => { 
+    if (currentValue.length === 2){
+      const kartaPierwsza = currentValue[0]
+      const kartaDruga = currentValue[1]
+      if(kartaPierwsza.faceValue === kartaDruga.faceValue){
+        TablicaKart.value[kartaPierwsza.position].dopasowana = true
+        TablicaKart.value[kartaDruga.position].dopasowana = true
+      } else {
+      setTimeout( () => {
+          TablicaKart.value[kartaPierwsza.position].visible = false
+          TablicaKart.value[kartaDruga.position].visible = false
+        }, 2000)
+      }
+      rekaGracza.value.length = 0}
+    }, {deep: true})
+
     return{
       TablicaKart,
-      odwrocKarte
+      odwrocKarte,
+      rekaGracza
     }
   }
 }
@@ -38,6 +68,7 @@ export default {
   @wybierz-karte="odwrocKarte"
   :position = "karta.position"/>
 </section>
+<h2>{{ rekaGracza }}</h2>
 </template>
 
 <style scoped>
